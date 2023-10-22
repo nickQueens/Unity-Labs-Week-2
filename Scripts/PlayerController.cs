4b8a4aa365185e53712eb4e3f7e9d3ce16e69b4d
nickQueens <146036623+nickQueens@users.noreply.github.com>
2023-10-22 22:06:53 +0100

    Update PlayerController.cs
    Move in z direction

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public float zBoundary = 15;
 
     public GameObject projectilePrefab;
     // Start is called before the first frame update
@@ -23,13 +25,30 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        } else if (transform.position.x > xRange)
+        }
+        else if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        // Limit z position
+        if (transform.position.z < 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+        else if (transform.position.z > zBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundary);
+        }
+
+
         // Move in x axis
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // Move in y axis
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
